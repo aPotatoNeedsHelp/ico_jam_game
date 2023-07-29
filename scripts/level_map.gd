7a9f5336bc344dce276e9ac0b7abee7da722442f
MikMik-dav <mimi.3d.001@gmail.com>
2023-07-29 23:52:24 +0200

    changed the foldering

--- scripts/level_map.gd
+++ scripts/level_map.gd
@@ -4,7 +4,7 @@ extends TileMap
 func _ready() -> void:
 	for cell in get_used_cells(0):
 		var scene_name = get_cell_tile_data(0, cell).get_custom_data("scene_name")
-		var scene = load(str("res://scenes/puzzle/", scene_name,".tscn")).instantiate()
+		var scene = load(str("res://", scene_name,"/", scene_name,".tscn")).instantiate()
 		scene.position = map_to_local(cell)
 		add_child(scene)
 		#clear cell
